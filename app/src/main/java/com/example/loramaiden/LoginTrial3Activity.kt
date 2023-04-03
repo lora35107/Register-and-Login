bdf6f67d1161fd8c79987b92cfe9b2d6273a608c
lora35107 <immaculatelora@gmail.com>
2023-04-03 12:10:01 +0300

    created simple login and register interfaces

---
+++ app/src/main/java/com/example/loramaiden/LoginTrial3Activity.kt
@@ -0,0 +1,11 @@
+package com.example.loramaiden
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginTrial3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_trial3)
+    }
+}
