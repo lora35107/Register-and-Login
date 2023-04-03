e16cae7fabb2e4d6f8076d20556858467f1f838b
lora35107 <immaculatelora@gmail.com>
2023-04-03 13:41:46 +0300

    added a function to the logi button

--- app/src/main/java/com/example/loramaiden/RegisterActivity3.kt
+++ app/src/main/java/com/example/loramaiden/RegisterActivity3.kt
@@ -2,10 +2,25 @@ package com.example.loramaiden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity3 : AppCompatActivity() {
+    private lateinit var name: EditText
+    private lateinit var telephone: EditText
+    private lateinit var email: EditText
+    private lateinit var passcode: EditText
+    private lateinit var register: Button
+    private lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register3)
+        name = findViewById(R.id.edtname)
+        telephone = findViewById(R.id.edttelephone)
+        email = findViewById(R.id.edtemail)
+        passcode = findViewById(R.id.edtpasscode)
+        login = findViewById(R.id.btnlogin)
+        register = findViewById(R.id.btnreg)
+
     }
 }
--- app/src/main/java/com/example/loramaiden/RelativeLayoutActivity.kt
+++ app/src/main/java/com/example/loramaiden/RelativeLayoutActivity.kt
@@ -2,10 +2,24 @@ package com.example.loramaiden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RelativeLayoutActivity : AppCompatActivity() {
+    private lateinit var email:EditText
+    private lateinit var password: EditText
+    private lateinit var loginbtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
+        email = findViewById(R.id.edt_email)
+        password = findViewById(R.id.edt_passcode)
+        loginbtn = findViewById(R.id.btn_login)
+
+        loginbtn.setOnClickListener {
+            Toast.makeText(this, "Trying to log in", Toast.LENGTH_SHORT).show()
+        }
     }
 }
